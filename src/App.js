1a3ad18f3e6c5608d31622ee70a411df8188509e
unknown <kelvin-joel>
2022-03-13 21:07:40 -0500

    buscador de imagenes con pixabay api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
-
+import Navbar from './Components/Navbar.jsx'
+import useStore from './Store/index'
+import ListImage from './Components/ListImage.jsx'
 function App() {
+
+  const { FecthData, loading } = useStore();
+
+  useEffect(() => {
+    FecthData('paisajes');
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <ListImage />
+      {/* {loading ? <p>Cargando</p> : ''} */}
     </div>
   );
 }
